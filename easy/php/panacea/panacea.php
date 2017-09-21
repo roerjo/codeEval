1e245e6860701da97cfbaf7525160685036b3a41
roerjo <roerjo.personal@gmail.com>
2017-09-20 20:03:50 -0400

    comment on the pancea problem

--- easy/php/panacea/panacea.php
+++ easy/php/panacea/panacea.php
@@ -4,13 +4,17 @@ $file = fopen($argv[1], 'r');
 
 while (($line = fgets($file)) !== false) {
 
+    // Break up the 'virus' and 'anti-virus' into an array
     $splitLine = explode("|", $line);
 
     $hexArray = explode(" ", trim($splitLine[0]));
 
     $binArray = explode(" ", trim($splitLine[1]));
 
+    // Initialize the sums (codeEval didn't like when I tried
+    // to set the binary sum = 0b0 for some reason
     $sumHex = 0x0;
+
     $sumBin = bindec(0);
 
     foreach ($hexArray as $hex) {
@@ -29,6 +33,7 @@ while (($line = fgets($file)) !== false) {
         echo "True\n";
     else
         echo "False\n";
+
 } 
 
 fclose($file);
